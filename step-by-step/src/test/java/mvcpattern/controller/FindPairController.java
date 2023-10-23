a719189bdb1db3d7959002c6c3ffd01124210044
sunghun <nokdonwithkj211@gmail.com>
2023-10-23 21:13:30 +0900

    chore(FindPairController): 부족한 부분 확인

--- step-by-step/src/test/java/mvcpattern/controller/FindPairController.java
+++ step-by-step/src/test/java/mvcpattern/controller/FindPairController.java
@@ -17,6 +17,7 @@ public class FindPairController extends AbstractController {
     @Override
     public void doProcess(Map<String, Object> model) {
         PairMatchingInfo pairMatchingInfo = (PairMatchingInfo) model.get("pairMatchingInfo");
+        // 존재하는 경우 추가 처리해야 한다
         model.put("machedPairNames", pairMatchingRepository.findAllPairNamesByPairMatchingInfo(pairMatchingInfo));
 
         outputView.print(model);
