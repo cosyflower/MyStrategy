f747fd2939cceffb3fb90105b0164226c3f92394
sunghun <nokdonwithkj211@gmail.com>
2023-12-04 16:57:27 +0900

    feat(DiscountPrice): 할인 금액을 관리하는 클래스 추가

--- step-by-step/src/main/java/refactorChristmas/DiscountPrice.java
+++ step-by-step/src/main/java/refactorChristmas/DiscountPrice.java
@@ -1,4 +1,9 @@
 package refactorChristmas;
 
 public class DiscountPrice {
+    private final int DiscountPriceValue;
+
+    public DiscountPrice(int discountPriceValue) {
+        DiscountPriceValue = discountPriceValue;
+    }
 }
